5673b2ded60409ec7b051a69b4c231809d14c810
Andre Bennett <101653602+AndreBennett@users.noreply.github.com>
2024-01-04 11:23:23 -0500

    Made Changes to App.jsx, not finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-
-import './App.css'
+import { puppyList } from "./data.js";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-
+  console.log("puppyList: ", puppyList);
   return (
     <>
+      <div className="App">
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
